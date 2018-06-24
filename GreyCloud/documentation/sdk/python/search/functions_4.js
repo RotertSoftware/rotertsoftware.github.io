ab96c2a8899a67c5ced56d40deae2370b9d88aff
DavidRotert <d.rotert@outlook.de>
2018-06-24 19:09:17 +0200

    1.0.0.0 dev 4 beta

--- GreyCloud/documentation/sdk/python/search/functions_4.js
+++ GreyCloud/documentation/sdk/python/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['parse',['parse',['../class_grey_cloud_1_1_a_p_i_1_1service___x_m_l_1_1_service_x_m_l.html#a6485f713315fe955724eaf98b34c5134',1,'GreyCloud::API::service_XML::ServiceXML']]],
+  ['parse',['parse',['../class_grey_cloud_1_1_a_p_i_1_1service___x_m_l_1_1_service_x_m_l.html#a6485f713315fe955724eaf98b34c5134',1,'GreyCloud.API.service_XML.ServiceXML.parse(self)'],['../class_grey_cloud_1_1_a_p_i_1_1service___x_m_l_1_1_service_x_m_l.html#a6485f713315fe955724eaf98b34c5134',1,'GreyCloud.API.service_XML.ServiceXML.parse(self)']]],
   ['post_5frequest',['POST_request',['../class_grey_cloud_1_1_a_p_i_1_1base_1_1_service_base_class.html#a731647d50d31508fef942a4a47f9e796',1,'GreyCloud.API.base.ServiceBaseClass.POST_request(self, path, arguments={}, allowMultipleArgs=False)'],['../class_grey_cloud_1_1_a_p_i_1_1base_1_1_service_base_class.html#a731647d50d31508fef942a4a47f9e796',1,'GreyCloud.API.base.ServiceBaseClass.POST_request(self, path, arguments={}, allowMultipleArgs=False)']]]
 ];
