9e2aa8296f25649ca2eb84f0809e71f6a6ae39b3
DavidRotert <d.rotert@outlook.de>
2018-07-10 11:42:20 +0200

    Update Responsive

---
+++ GreyCloud/documentation/GreyCloud/_vti_cnf/namespace_grey_cloud_1_1filemanagement_1_1models.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 Jun 2018 16:57:46 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|CDEVLABLAPTOP\\David Rotert
+vti_modifiedby:SR|CDEVLABLAPTOP\\David Rotert
+vti_timecreated:TR|24 Jun 2018 16:57:46 -0000
+vti_cacheddtm:TX|24 Jun 2018 16:57:46 -0000
+vti_filesize:IR|159
+vti_backlinkinfo:VX|
