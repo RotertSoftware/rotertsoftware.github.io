f8538e21996dc15666e547993cedbd618b967258
DavidRotert <d.rotert@outlook.de>
2018-08-22 13:34:43 +0200

    Added privacy terms and contact page

--- GreyCloud/js/_vti_cnf/custom.js
+++ GreyCloud/js/_vti_cnf/custom.js
@@ -1,4 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|05 Jun 2017 23:19:04 -0000
+vti_timelastmodified:TR|22 Aug 2018 09:33:43 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|GreyCloud/index.html
+vti_backlinkinfo:VX|GreyCloud/privacy.html GreyCloud/contact.html GreyCloud/index.html
+vti_author:SR|CDEVLABLAPTOP\\David Rotert
+vti_modifiedby:SR|CDEVLABLAPTOP\\David Rotert
+vti_timecreated:TR|05 Jun 2017 23:19:04 -0000
+vti_nexttolasttimemodified:TW|22 Aug 2018 09:27:15 -0000
+vti_cacheddtm:TX|22 Aug 2018 09:27:15 -0000
+vti_filesize:IR|442
