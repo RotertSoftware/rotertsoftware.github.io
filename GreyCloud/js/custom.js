7d0b30d9ef8835335f4e5611a717ab5c272e6668
DavidRotert <d.rotert@outlook.de>
2018-08-22 18:28:00 +0200

    Updated to final website version

--- GreyCloud/js/custom.js
+++ GreyCloud/js/custom.js
@@ -1,7 +1,7 @@
 // preloader
-$(window).load(function(){
+/*$(window).load(function(){
     $('.preloader').fadeOut(1000); // set duration in brackets    
-});
+});*/
 
 $(function() {
     new WOW().init();
